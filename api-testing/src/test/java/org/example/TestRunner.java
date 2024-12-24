f45a770ba02b918c2eb87884540d61a754718763
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2024-12-25 00:30:58 +0530

    written example test using cucumber

---
+++ api-testing/src/test/java/org/example/TestRunner.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "org.example.definitions",
+        plugin = {"pretty"}
+)
+//Run this file to execute tests
+public class TestRunner{
+
+}
---
+++ api-testing/src/test/resources/features/Book.feature
@@ -0,0 +1,6 @@
+Feature: Book Details
+
+  @GetAllBooks
+  Scenario: Get all books
+    Given I send a request to get all books
+    Then the API should return status 200
