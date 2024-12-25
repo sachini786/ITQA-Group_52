3cb99c732a7e38d956eeb1a8013bfd290649d9f0
Nadun Atugoda <nadunatugoda123@gmail.com>
2024-12-26 00:19:55 +0530

    deletion written for the testing

---
+++ api-testing/src/test/java/starter/stepdefinitions/BookDeletion.java
@@ -0,0 +1,51 @@
+package starter.stepdefinitions;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+// import io.restassured.RestAssured;
+// import io.restassured.authentication.PreemptiveBasicAuthScheme;
+// import java.io.IOException;
+// import io.cucumber.java.AfterAll;
+// import io.cucumber.java.BeforeAll;
+import io.restassured.response.Response;
+import net.serenitybdd.rest.SerenityRest;
+
+
+public class BookDeletion {
+    public Response response;
+    // private static Process process;
+
+    @Given("The book is needed to be deleted")
+    public void the_book_is_needed_to_be_deleted(){
+        response = SerenityRest.given()
+                .when()
+                .delete("/books/1");
+    }
+    @When("I delete a book which is already deleted")
+    public void i_delete_a_book_which_is_already_deleted(){
+        String requestBody = """
+            {
+                "id": 1,
+                "title": "Update Book title01"
+                "author": "rowan atkinson"
+    
+            }
+            """;
+
+    response = SerenityRest.given()
+            .header("Content-Type", "application/json")
+            .body(requestBody)
+            .when()
+            .delete("/books/1");
+    }
+    @Then("the response status should be the {int}")
+    public void the_response_status_should_be_the(int status){
+        SerenityRest.restAssuredThat(response->response.statusCode(status));
+    }
+}
+
+
+
+
---
+++ api-testing/src/test/resources/features/Delete.feature
@@ -0,0 +1,6 @@
+Feature: Delete a book
+   @DeleteBook
+   Scenario: Delete a Book which is already deleted
+    Given The book is needed to be deleted
+    When I delete a book which is already deleted
+    Then the response status should be the 403
