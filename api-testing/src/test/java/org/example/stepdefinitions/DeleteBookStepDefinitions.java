fc2f9c0613ebeaf7e5b50072a0c10612ed4f78a0
PasinduTY <pasindu.t.y.s@gmail.com>
2025-01-02 17:20:04 +0530

    DeleteBook as user & admin

--- api-testing/src/test/java/org/example/stepdefinitions/DeleteBookStepDefinitions.java
+++ api-testing/src/test/java/org/example/stepdefinitions/DeleteBookStepDefinitions.java
@@ -1,52 +1,23 @@
 package org.example.stepdefinitions;
 
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
-
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-
+import io.restassured.response.Response;
+import net.serenitybdd.rest.SerenityRest;
 
 public class DeleteBookStepDefinitions {
-    private String bookId; // Book ID to delete
-    private int statusCode; // Response status code
-    //private int responseStatus;
-    private static final String BASE_URL = "http://localhost:7081/api";
+
+    public Response response;
 
     @When("I send a DELETE request to delete a book with ID {int}")
-    public void i_send_a_delete_request_to_delete_a_book_with_id(Integer id) {
-        // Store the ID
-        String bookId = id.toString();
-
-
-        // Send the DELETE request and store the response status
-        statusCode = given().baseUri(BASE_URL)
-                .auth().basic("user", "password") // Use normal user credentials
-                .when()
-                .delete("/books/" + bookId) // Perform DELETE request
-                .getStatusCode(); // Store the status code
+    public void iSendRequestToDeleteBooks(int bookId) {
+        response = SerenityRest.when()
+                .delete("/books/" + bookId);
     }
 
     @Then("the response status should be {int}")
-    public void the_response_status_should_be(Integer expectedStatusCode) {
-        if (statusCode != expectedStatusCode) {
-            throw new AssertionError(
-                    "Expected status code " + expectedStatusCode + " but got " + statusCode
-            );
-        }
-        System.out.println("Delete request resulted in status code: " + statusCode);
+    public void responseStatusCodeShouldBe(int int1) {
+        SerenityRest.restAssuredThat(response->response.statusCode(int1));
     }
 
-//    @Then("the book should still exist")
-//    public void the_book_should_still_exist() {
-//        // Send a GET request to check if the book still exists
-//        given()
-//                .auth().basic("user", "password") // Admin credentials to verify book existence
-//                .when()
-//                .get("/books/" + bookId)
-//                .then()
-//                .statusCode(200) // Expecting 200 because the book should still exist
-//                .body("id", equalTo(Integer.parseInt(bookId))); // Validate book ID matches
-//    }
 }
--- api-testing/src/test/resources/features/DeleteBook.feature
+++ api-testing/src/test/resources/features/DeleteBook.feature
@@ -2,9 +2,14 @@ Feature: Delete book by ID
   @api
   Scenario: Delete a book with a valid ID as a regular user
     Given I am a normal user
-    When I send a DELETE request to delete a book with ID 1
+    When I send a DELETE request to delete a book with ID 4
+    Then the response status should be 403
+
+  @api
+  Scenario: Delete a book with a valid ID as a admin user
+    Given I am an admin user
+    When I send a DELETE request to delete a book with ID 5
     Then the response status should be 200
-#    And the book should still exist
 
 
 
