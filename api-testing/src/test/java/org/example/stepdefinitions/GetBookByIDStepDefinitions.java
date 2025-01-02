4d37f246e8f68443c71363c8f0cdda1c2f4678db
Nimeesha02 <nimee.fdo@gmail.com>
2025-01-03 00:24:31 +0530

    Update GetBookByID

--- api-testing/src/test/java/org/example/stepdefinitions/GetBookByIDStepDefinitions.java
+++ api-testing/src/test/java/org/example/stepdefinitions/GetBookByIDStepDefinitions.java
@@ -1,37 +1,121 @@
 package org.example.stepdefinitions;
 
-import io.cucumber.java.en.And;
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.serenitybdd.rest.SerenityRest;
+import io.restassured.mapper.ObjectMapperType;
+import io.restassured.specification.RequestSpecification;
 import io.restassured.response.Response;
+import net.serenitybdd.rest.SerenityRest;
+import org.example.models.Book;
 
-import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class GetBookByIDStepDefinitions {
     private Response response;
+    private int createBookId;
+    private RequestSpecification requestSpecification;
+    private static final String BASE_URL = "http://localhost:7081/";
 
-    @When("I send a GET request to retrieve the book with ID {int}")
-    public void iSendAGetRequestToRetrieveTheBookWithID(int bookId) {
-        response = SerenityRest.when()
-                .get("/books/" + bookId);
+    @Given("user authenticate using by {string} and {string} wants to retrieve the book with valid ID")
+    public void userUsernameAndPassword(String username, String password) {
+        requestSpecification = SerenityRest.given()
+                .baseUri(BASE_URL)
+                .auth()
+                .basic(username, password);
     }
 
-    @Then("The API response status code should be {int}")
-    public void responseStatusCodeForBookByIDShouldBe(int statusCode) {
-        restAssuredThat(response -> response.statusCode(statusCode));
+    @Given("admin user wants to get book the ID is valid")
+    public void adminUserWantsToGetBookWithValidID() {
+        userUsernameAndPassword("admin", "password");
     }
 
-    @And("The response should contain the book details with ID {int}")
-    public void theResponseShouldContainTheBookDetails(int bookId) {
-        response.then().body("id", equalTo(bookId));
-        response.then().body("title", notNullValue());
-        response.then().body("author", notNullValue());
+    @Given("regular user wants to get book the ID is valid")
+    public void regularUserWantsToGetBookWithValidID() {
+        userUsernameAndPassword("user", "password");
+    }
 
-        //String title = response.jsonPath().getString("title");
-        //String author = response.jsonPath().getString("author");
-        //System.out.println("Retrieved Book Details - ID: " + bookId + ", Title: " + title + ", Author: " + author);
+    @Given("admin user wants to get a non-existent book")
+    public void adminUserWantsToGetNonExistentBook() {
+        userUsernameAndPassword("admin", "password");
+    }
+
+    @Given("regular user wants to get a non-existent book")
+    public void regularUserWantsToGetNonExistentBook() {
+        userUsernameAndPassword("user", "password");
+    }
+
+    @Given("admin user wants to get a book with an invalid ID")
+    public void adminUserWantsToGetBookWithInvalidID() {
+        userUsernameAndPassword("admin", "password");
+    }
+
+    @Given("regular user wants to get a book with an invalid ID")
+    public void regularUserWantsToGetBookWithInvalidID() {
+        userUsernameAndPassword("user", "password");
+    }
+
+    @Given("the book exists")
+    public void hasBooks() {
+        userUsernameAndPassword("user","password");
+        final String REQUEST_BODY= """
+                    {
+                        "title": "Harry Potter",
+                        "author": "JK Rolling"
+                    }
+                """;
+
+        createBookId= requestSpecification
+                .body(REQUEST_BODY)
+                .header("Content-Type", "application/json")
+                .post("/api/books").getBody().as(Book.class, ObjectMapperType.JACKSON_2).id();
+        System.out.println("Created Book ID: " + createBookId);
+    }
+
+    @After("@ValidIdFormat-Admin")
+    public void afterBooksExistScenario1() {
+        requestSpecification.when()
+                .delete("/api/books/" + createBookId);
+    }
+
+    @After("@ValidIdFormat-RegularUser")
+    public void afterBooksExistScenario2() {
+        requestSpecification.when()
+                .delete("/api/books/" + createBookId);
+    }
+
+    @When("user calls service to get the book with ID")
+    public void userCallsServiceToGetBookWithID() {
+        response = requestSpecification.when().get("/api/books/" + createBookId);
+    }
+
+    @When("user calls service to get the book with ID {int}")
+    public void userCallsServiceToGetBookWithSpecificID(int bookId) {
+        response = requestSpecification.when().get("/api/books/" + bookId);
+    }
+
+    @When("user calls service to get the book with ID {string}")
+    public void userCallsServiceToGetBookWithInvalidID(String bookId) {
+        response = requestSpecification.when().get("/api/books/" + bookId);
+    }
+
+    @Then("user expects response status to be {int}")
+    public void responseStatusCodeShouldBe(int statusCode) {
+        response.then().statusCode(statusCode);
+    }
+
+    @Then("user expects response should contain the book details with ID")
+    public void responseShouldContainBookDetails() {
+        response.then()
+                .body("id", equalTo(createBookId))
+                .body("title", notNullValue())
+                .body("author", notNullValue());
+    }
+
+    @Then("user expects response should contain a {string} message")
+    public void responseShouldContainMessage(String expectedMessage) {
+        response.then().body(equalTo(expectedMessage));
     }
 }
--- api-testing/src/test/resources/features/GetBookByID.feature
+++ api-testing/src/test/resources/features/GetBookByID.feature
@@ -1,7 +1,55 @@
 Feature: GET a book by ID
 
-  @api
-  Scenario: GET a valid book by ID
-    When I send a GET request to retrieve the book with ID 1
-    Then The API response status code should be 200
-    And The response should contain the book details with ID 1
+  Scenario Outline: GET a book by valid ID as different user when book does not exists
+    Given user authenticate using by "<username>" and "<password>" wants to retrieve the book with valid ID
+    When user calls service to get the book with ID
+    Then user expects response status to be <statusCode>
+
+    Examples:
+      | username | password | statusCode |
+      | admin    | password | 200        |
+      | user     | password | 200        |
+      | test     | test     | 401        |
+
+
+  @ValidIdFormat-Admin
+  Scenario: GET a book with a valid ID format as an admin user when book exists
+    Given admin user wants to get book the ID is valid
+    Given the book exists
+    When user calls service to get the book with ID
+    Then user expects response status to be 200
+    And user expects response should contain the book details with ID
+
+  @ValidIdFormat-RegularUser
+  Scenario: GET a book with a valid ID format as an regular user when book exists
+    Given regular user wants to get book the ID is valid
+    Given the book exists
+    When user calls service to get the book with ID
+    Then user expects response status to be 200
+    And user expects response should contain the book details with ID
+
+  @BookNotFound
+  Scenario: GET a non-existent book by ID as an admin
+    Given admin user wants to get a non-existent book
+    When user calls service to get the book with ID 9999
+    Then user expects response status to be 404
+    And user expects response should contain a "Book not found" message
+
+  @BookNotFound
+  Scenario: GET a non-existent book by ID as a regular user
+    Given regular user wants to get a non-existent book
+    When user calls service to get the book with ID 9999
+    Then user expects response status to be 404
+    And user expects response should contain a "Book not found" message
+
+  @InvalidIDFormat
+  Scenario: GET a book with an invalid ID format as an admin
+    Given admin user wants to get a book with an invalid ID
+    When user calls service to get the book with ID "invalid"
+    Then user expects response status to be 400
+
+  @InvalidIDFormat
+  Scenario: GET a book with an invalid ID format as a regular user
+    Given regular user wants to get a book with an invalid ID
+    When user calls service to get the book with ID "invalid"
+    Then user expects response status to be 400
