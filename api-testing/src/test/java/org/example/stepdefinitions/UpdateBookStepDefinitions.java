a83b94147254986b033f7a802678c131d4a6a643
Amal Shalinda <Shalindajda99@gmail.com>
2025-01-02 19:37:05 +0530

    remove authstepFile

--- api-testing/src/test/java/org/example/stepdefinitions/UpdateBookStepDefinitions.java
+++ api-testing/src/test/java/org/example/stepdefinitions/UpdateBookStepDefinitions.java
@@ -4,41 +4,68 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import net.serenitybdd.rest.SerenityRest;
 
-public class UpdateBookStepDefinitions{
+public class UpdateBookStepDefinitions {
     public Response response;
 
-    @When("I send a PUT request to update the test book")
-    public void iSendRequestToUpdateBooks(){
-        response = SerenityRest.when()
-                .put("/books/1");
+    private RequestSpecification requestSpecification;
+    private static final String BASE_URL = "http://localhost:7081/api";
+
+    @When("I send a PUT request to update the test book with ID {int}")
+    public void iSendRequestToUpdateBooks(int bookID){
+        String requestBody ="""
+                {
+                "id": %d,
+                "title": "updated title01",
+                "author": "Updated author"
+                }
+                """;
+        response = requestSpecification.when()
+                .header("Content-Type", "application/json")
+                .body(requestBody.formatted(bookID))
+                .put("/books/" + bookID);
     }
 
-    @Then("the response status code should be as {int}")
-    public void responseStatusCodeShouldBe(Integer int1) {
-        SerenityRest.restAssuredThat(response->response.statusCode(int1));
-    }
 
-    @When("I send a PUT request to update the test book with missing mandatory fields")
-    public void iSendRequestToUpdateBooksWithMissingFields(){
-        response = SerenityRest.when()
-                .put("/books/1");
+
+    @When("I send a PUT request to update the test book with missing mandatory fields with ID {int}")
+    public void iSendRequestToUpdateBooksWithMissingFields(int bookID){
+        String requestBody ="""
+                {
+                "id": %d,
+                "author": "Updated author"
+                }
+                """;
+        response = requestSpecification.when()
+                .header("Content-Type", "application/json")
+                .body(requestBody.formatted(bookID))
+                .put("/books/" + bookID);
     }
     @Then("the response status code should be {int}")
     public void responseStatusCodeShouldBeExact(Integer expectedStatusCode) {
         SerenityRest.restAssuredThat(response -> response.statusCode(expectedStatusCode));
     }
 
-    @When("I send a PUT request to update the test book as a normal user")
-    public void iSendRequestToUpdateBooksAsNormalUser() {
-        response = SerenityRest.when()
-                .put("/books/1");
+    @Given("I am an admin user")
+    public void userNeedToUpdateABookAsAnAdmin() {
+
+        requestSpecification =  SerenityRest.given()
+                .baseUri(BASE_URL)
+                .auth()
+                .basic("admin","password")
+        ;
     }
 
-    @Then("the response status code should be in {int}")
-    public void responseStatusCodeShouldBeIn(Integer expectedStatusCode) {
-        SerenityRest.restAssuredThat(response -> response.statusCode(expectedStatusCode));
+    @Given("I am a normal user")
+    public void userNeedToUpdateABookAsAnNormalUser() {
+        requestSpecification =  SerenityRest.given()
+                .baseUri(BASE_URL)
+                .auth()
+                .basic("user","password")
+        ;
     }
-
 }
+
+
--- api-testing/src/test/resources/features/UpdateBook.feature
+++ api-testing/src/test/resources/features/UpdateBook.feature
@@ -2,15 +2,15 @@ Feature: Update Book by ID
 
   Scenario: Update a book with a valid ID as an admin
     Given I am an admin user
-    When I send a PUT request to update the test book
-    Then the response status code should be as 200
+    When I send a PUT request to update the test book with ID 1
+    Then the response status code should be 200
 
   Scenario: Update a book with missing mandatory fields as an admin
     Given I am an admin user
-    When I send a PUT request to update the test book with missing mandatory fields
-    Then the response status code should be 404
+    When I send a PUT request to update the test book with missing mandatory fields with ID 2
+    Then the response status code should be 400
 
   Scenario: Update a book with a valid ID as a normal user
     Given I am a normal user
-    When I send a PUT request to update the test book as a normal user
-    Then the response status code should be in 403
+    When I send a PUT request to update the test book with ID 1
+    Then the response status code should be 403
