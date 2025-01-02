24946eddc33a3b78b70033a676e3f01ca0a60891
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2025-01-02 18:20:50 +0530

    api test done

--- api-testing/src/test/java/org/example/CucumberTestSuite.java
+++ api-testing/src/test/java/org/example/CucumberTestSuite.java
@@ -8,7 +8,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "classpath:features",
-        tags = "@api"
+        features = "classpath:features"
 )
 public class CucumberTestSuite {}
